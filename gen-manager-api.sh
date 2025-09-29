19d6e7492fc875a7b1c4f24dccaf54fe56933c39
ndhung <nguyenduyhung04092004@gmail.com>
2025-09-30 03:04:45 +0700

    [Remove] remove docs

--- gen-manager-api.sh
+++ gen-manager-api.sh
@@ -1,7 +1,5 @@
 mkdir -p manager-api/proto
-mkdir -p manager-api/docs
 rm -rf .manager-api/proto/*
-rm -rf .manager-api/docs/*
 protoc \
     -I=. \
     --go_out=. \
@@ -10,5 +8,3 @@ protoc \
     --openapiv2_out=manager-api/docs \
     --openapiv2_opt logtostderr=true \
     manager-api/api/*.proto
-cp manager-api/docs/manager-api/api/* manager-api/docs
-rm -rf manager-api/docs/manager-api
