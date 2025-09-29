095fa4e241a34eaeadec26fd12d7372da8d4099d
ndhung <nguyenduyhung04092004@gmail.com>
2025-09-29 23:37:26 +0700

    [Gen] gen code manager-api

---
+++ gen-manager-api.sh
@@ -0,0 +1,12 @@
+mkdir -p manager-api/proto
+mkdir -p manager-api/docs
+rm -rf .manager-api/proto/*
+rm -rf .manager-api/docs/*
+protoc \
+    -I=. \
+    --go_out=. \
+    --go-grpc_out=. \
+    --grpc-gateway_out=. \
+    --openapiv2_out=manager-api/docs \
+    --openapiv2_opt logtostderr=true \
+    manager-api/api/*.proto \
